9b5076f7a5fa32465ae2772ca78106386d207f5d
Noobmasta115 <diegmar.1215@gmail.com>
2024-02-10 18:41:56 -0600

    Prueba 2

--- Codigo/BaseDeDatos/BaseDeDatosProyecto.py
+++ Codigo/BaseDeDatos/BaseDeDatosProyecto.py
@@ -2,11 +2,9 @@ def GrabarFlujoYDistancia(flujo, distancia):
     import pymysql
     import datetime
 
-#En el host es la ip de tu raspberry y la contraseña tambien
-
     db = pymysql.connect(host='192.168.0.219', user='root', password='CPBraulio', db='AlmacenDeTemperaturas', charset='utf8')
     ahorita = datetime.datetime.now()
-    sql = f"INSERT INTO Datos (fecha, temperatura, humedad) VALUES ('{ahorita}', {flujo}, {distancia});"
+    sql = f"INSERT INTO Datos (fecha, flujo, distancia) VALUES ('{ahorita}', {flujo}, {distancia});"
     cur = db.cursor()
     cur.execute(sql)
     db.commit()
--- Codigo/BaseDeDatos/InformacionDeLosSensores.py
+++ Codigo/BaseDeDatos/InformacionDeLosSensores.py
@@ -1,9 +1,6 @@
 import ProyectoFinal
-flujo = 0
-distancia = 0
 
 def leerFlujoYDistancia():
     global flujo, distancia
-    import random
     flujo = ProyectoFinal.flow_rate
     distancia = ProyectoFinal.sensor
