a6fe17d0976fe1dd1ee1fc4c667f875e4e4a67b7
BraulioPerdomo <perdomobraulio15@gmail.com>
2024-02-10 18:22:09 -0600

    xd

--- Codigo/BaseDeDatos/BaseDeDatosProyecto.py
+++ Codigo/BaseDeDatos/BaseDeDatosProyecto.py
@@ -2,7 +2,9 @@ def GrabarFlujoYDistancia(flujo, distancia):
     import pymysql
     import datetime
 
-    db = pymysql.connect(host='192.168.100.129', user='root', password='N0s3w3y', db='AlmacenDeTemperaturas', charset='utf8')
+#En el host es la ip de tu raspberry y la contraseña tambien
+
+    db = pymysql.connect(host='192.168.0.219', user='root', password='CPBraulio', db='AlmacenDeTemperaturas', charset='utf8')
     ahorita = datetime.datetime.now()
     sql = f"INSERT INTO Datos (fecha, temperatura, humedad) VALUES ('{ahorita}', {flujo}, {distancia});"
     cur = db.cursor()
