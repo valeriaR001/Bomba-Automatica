7efc101e6d0a7952216c0a2963fd9c4de9fd2f0a
Noobmasta115 <diegmar.1215@gmail.com>
2024-02-10 18:36:35 -0600

    Prueba con sensores

--- Codigo/BaseDeDatos/InformacionDeLosSensores.py
+++ Codigo/BaseDeDatos/InformacionDeLosSensores.py
@@ -1,8 +1,9 @@
+import ProyectoFinal
 flujo = 0
 distancia = 0
 
 def leerFlujoYDistancia():
     global flujo, distancia
     import random
-    flujo = random.uniform(0.0, 30.0)
-    distancia = int(random.randrange(0,100, 1))
+    flujo = ProyectoFinal.flow_rate
+    distancia = ProyectoFinal.sensor
